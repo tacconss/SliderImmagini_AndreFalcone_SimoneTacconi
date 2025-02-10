09ec67bd1a59d6725691e2d24087d121b0d85aa2
AndreaFalcone1 <falconeandrea@itis-molinari.eu>
2025-02-10 10:50:48 +0100

    Navigator

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import { createNavigator } from "./navigator.js";
+import { createNavigator } from "./navigator/navigator.js";
 
 const navigator = createNavigator(document.querySelector("#container"));
 

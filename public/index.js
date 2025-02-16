40a5c991c5ee9f85962c02379108c9fccce90280
Tacconi <tacconisimone@itis-molinari.eu>
2025-02-16 16:16:37 +0100

    sistemazione index.html e index.js

--- public/index.js
+++ public/index.js
@@ -8,23 +8,59 @@ const login = generateLoginComponent(document.querySelector("#loginContainer"));
 const navigator = createNavigator(document.querySelector("#container"));
 const imageCarosel = generateImageCarosel(document.querySelector("#immaginiCarosello"))
 const table = generateTable(document.querySelector("#immaginiTabella"))
-const uploadImage = generateUploadImage();
 
-let imgList = ["https://blog.geografia.deascuola.it/uploads/2023/04/Val-d-Orcia-panorama_1bb3052e3a980627951e300394f187d8.jpg", "https://ilfotografo.it/wp-content/uploads/2022/06/CAN128.lead_.la_fiorita_98159931-1024x683.jpg","https://i0.wp.com/fotografinviaggio.it/wp-content/uploads/2023/04/Immagine-copertina-per-blog-1-2-2.jpg?fit=1000%2C662&ssl=1","https://www.emotionrit.it/wp-content/uploads/2022/08/Perche-un-panorama-ci-colpisce-scaled.jpg"];
+const uploadButton = document.querySelector("#uploadButton");
+const inputFile = document.querySelector('#file');
 
-const test = (json) => {
-    console.log(json);
+let list = []
+
+const uploadFunction = async function() {
+    const formData = new FormData();
+    formData.append("file", inputFile.files[0]);
+    try {
+        const res = await fetch("/upload", {
+            method: 'POST',
+            body: formData
+        });
+        const data = await res.json();
+        //Check if URL is duplicate
+        let check = false;
+        list.forEach((element) => {
+            if (element == data.url) {
+                check = true;
+            }
+        });
+        //If it's not duplicate
+        if (!check) {
+            list.push(data);
+        }
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-uploadImage.uploadImage();
-console.log(uploadImage.returnImageList());
+uploadButton.onclick = async function () {
+    await uploadFunction();
+    fetch('/get').then((res) => res.json()).then((imgs) => {
+        //Set img list
+        table.setList(imgs);
+        imageCarosel.setImgList(imgs);
+        //Render
+        table.render(imageCarosel);
+        imageCarosel.render();
+    })
+}
 
-//Fare che prende i file scaricati su FS e caricarli nella tabella e carosello
-table.setList(imgList);
-imageCarosel.setImgList(imgList);
+fetch('/get').then((res) => res.json()).then((imgs) => {
+    //Set img list
+    table.setList(imgs);
+    imageCarosel.setImgList(imgs);
+    //Render
+    table.render(imageCarosel);
+    imageCarosel.render();
+})
 
-
-table.render();
+table.render(imageCarosel);
 imageCarosel.render();
 
 login.build("453f3d23-1478-440e-ada7-f2ebdcd16b14", "private");
